8c22335f679309262e3fd4e461d53b5cfae7ca87
cridder <cjridder@gmail.com>
2022-12-01 19:27:56 -0800

    added psuedo code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,18 @@
-var deletethis;
+// psuedo code
 
-//akfhdakdhfakjshf
-//kagdsfuihasdfjkhasdkjf
+// button function, when button click it does what?
+// gets random choice from array returned from jason if last visit was more than 24 hours ago, else display the last result and say come back tommorow!
+
+// get random number from 0 to 78
+
+// select card from array with the random number
+
+// hide button
+
+// show card in a 'card' 
+
+// add card info below/in the 'card', arcana, suit, fortune telling, keywords, meanings; light and/or shadow, numerology, astrology, affirmation, questions to ask.
+
+// for future maybe, use the reverse, create function for light and shadow
+
+// Store result in persistent data for today and the last 6 days/visits, with date visited maybe
