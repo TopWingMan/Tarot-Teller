0ddf8f1710c8d937fdfba973786ebe746d510f9b
jib180 pimpslaps.you <zacharystanaford@gmail.com>
2022-12-01 19:42:16 -0800

    added fetch that finds image and fortune description of card

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,16 @@
 // get random number from 0 to 78
 
 // select card from array with the random number
+const tarotData = (src="./assets/json/tarot-images.json");
+
+//Find Card Image
+fetch (tarotData)
+.then((response) => response.json())
+.then((data) =>
+{
+    console.log(data.cards[0].img);
+    console.log(data.cards[0].fortune_telling[0]);
+});
 
 // hide button
 
