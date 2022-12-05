9961aa0b70007d1a6b1e2bce105c2db42f675040
jib180 pimpslaps.you <zacharystanaford@gmail.com>
2022-12-04 18:27:59 -0800

    added button functonality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,32 @@
 // psuedo code
 
-// button function, when button click it does what?
-// gets random choice from array returned from jason if last visit was more than 24 hours ago, else display the last result and say come back tommorow!
+const tarotData = (src="./assets/json/tarot-images.json");
 
 // get random number from 0 to 78
 var randomNumber = (Math.floor(Math.random() * (78 - 0)));
 
-// select card from array with the random number
-const tarotData = (src="./assets/json/tarot-images.json");
-
-//Find Card Image
-fetch (tarotData)
-.then((response) => response.json())
-.then((data) =>
+// button function, when button click it does what?
+document.getElementById("button").addEventListener("click", function()
 {
-    console.log(data.cards[randomNumber].name);
-    console.log(data.cards[randomNumber].img);
-    console.log(data.cards[randomNumber].fortune_telling[0]);
-});
+    //Get card information
+    fetch (tarotData)
+    .then((response) => response.json())
+    .then((data) =>
+    {
+        console.log(data.cards[randomNumber].name);
+        console.log(data.cards[randomNumber].img);
+        console.log(data.cards[randomNumber].fortune_telling[0]);
+    });
+
+    DisplayCard(data.cards[randomNumber].img);
+})
+
+function DisplayCard(img)
+{
+    var imgPath = img;
+}
+
+// gets random choice from array returned from jason if last visit was more than 24 hours ago, else display the last result and say come back tommorow!
 
 // hide button
 
