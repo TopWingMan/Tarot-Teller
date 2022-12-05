422449f8afb34ae7c28b76b175869d330d9a17c2
jib180 pimpslaps.you <zacharystanaford@gmail.com>
2022-12-04 19:38:33 -0800

    refactored js, added StoreData function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,30 @@
-// psuedo code
-
+const history = ["currentDay", "-1", "-2", "-3", "-4", "-5"]; //Find a way to store this as persisitent data
 const tarotData = (src="./assets/json/tarot-images.json");
 
-// get random number from 0 to 78
-var randomNumber = (Math.floor(Math.random() * (78 - 0)));
+//pick a random number from 0 to 78
+var chosenCardNumber = (Math.floor(Math.random() * (78 - 0)));
 
 // button function, when button click it does what?
 document.getElementById("button").addEventListener("click", function()
 {
+    //Hide Button
+
     //Get card information
     fetch (tarotData)
     .then((response) => response.json())
     .then((data) =>
     {
-        console.log(data.cards[randomNumber].name);
-        console.log(data.cards[randomNumber].img);
-        console.log(data.cards[randomNumber].fortune_telling[0]);
-        DisplayCard(data.cards[randomNumber].img);
+        console.log(data.cards[chosenCardNumber].name);
+        console.log(data.cards[chosenCardNumber].img);
+        console.log(data.cards[chosenCardNumber].fortune_telling[0]);
+
+        //Display card img
+        DisplayCard(data.cards[chosenCardNumber].img);
+
+        //Display card fortune description
+
+        //Store card data as persistent data
+        StoreData(chosenCardNumber);
     });
 })
 
@@ -28,14 +36,14 @@ function DisplayCard(imgVar)
     src.appendChild(img);
 }
 
+function StoreData(StoreData)
+{
+    history[0] = StoreData;
+    console.log(StoreData);
+}
+
 // gets random choice from array returned from jason if last visit was more than 24 hours ago, else display the last result and say come back tommorow!
-
-// hide button
-
-// show card in a 'card' 
 
 // add card info below/in the 'card', arcana, suit, fortune telling, keywords, meanings; light and/or shadow, numerology, astrology, affirmation, questions to ask.
 
 // for future maybe, use the reverse, create function for light and shadow
-
-// Store result in persistent data for today and the last 6 days/visits, with date visited maybe
